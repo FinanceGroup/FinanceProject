6bed383448cec0f95e931ed6cea1b52a19c7fb62
guxiaoliang <guxiaoliang0208@126.com>
2016-07-17 23:30:30 +0800

    initi currency module

---
+++ Finance.Standlone/Finance.Standlone/wwwroot/scripts/currency/currency.js
@@ -0,0 +1,21 @@
+﻿define(['jquery', 'kendo', 'currencyGrid','events'], function
+    ($, kendo, payupGrid, ginniePayupGrid, events) {
+    function init(options) {
+        events.publish('app.change_main_view_title', '币别维护');
+        options.pathChanged && kendo.ui.progress($(document.body), false);
+        payupGrid.init(options);
+
+        $("#payup_wrapper").kendoSplitter({
+            orientation: 'vertical',
+            panes: [
+                { size: "50%", max: "90%" },
+                { size: "50%", max: "90%" }
+            ],
+        });
+    }
+    function unactive() {
+        payupGrid.unactive();
+        //ginniePayupGrid.unactive();
+    }
+    return { init: init, unactive: unactive };
+});
