0b861764e73040f4974bdef2e9b20e491226b329
nanlengyue <541896197@qq.com>
2016-07-17 20:55:43 +0800

    init JS

---
+++ Finance.Standlone/Finance.Standlone/wwwroot/scripts/Payup.js
@@ -0,0 +1,24 @@
+/**
+ * Created by GYN on 2016/7/11.
+ */
+define(['jquery', 'kendo', 'payupGrid', 'ginniePayupGrid', 'events'], function
+    ($, kendo, payupGrid, ginniePayupGrid, events){
+    function init(options){
+        events.publish('app.change_main_view_title', 'Payup Grid');
+        options.pathChanged && kendo.ui.progress($(document.body), false);
+        payupGrid.init(options);
+
+        $("#payup_wrapper").kendoSplitter({
+            orientation: 'vertical',
+            panes: [
+                {size: "50%", max: "90%"},
+                {size: "50%", max: "90%"}
+            ],
+        });
+    }
+    function unactive(){
+        payupGrid.unactive();
+        ginniePayupGrid.unactive();
+    }
+    return {init: init, unactive: unactive};
+});
